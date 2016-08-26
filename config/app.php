6a768091895f73df73d332045e6272e9fd35daaa
AleksandrMishchaniuk <map.m@ua.fm>
2016-08-26 13:55:41 +0300

    added debugbar and html-helpers

--- config/app.php
+++ config/app.php
@@ -144,7 +144,12 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        
+        Collective\Html\HtmlServiceProvider::class,
+        
+        //dev
+        Barryvdh\Debugbar\ServiceProvider::class,
+        
     ],
 
     /*
@@ -192,6 +197,12 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        
 
     ],
 
