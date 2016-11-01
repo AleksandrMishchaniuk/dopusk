a55ecce665a21edfa20e980ab4d07d7ddf85efdf
AleksandrMishchaniuk <map.m@ua.fm>
2016-11-01 09:29:46 +0200

    created tolerance seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call('RangesTableSeeder');
         $this->call('QualitiesTableSeeder');
         $this->call('FieldsTableSeeder');
+        $this->call('TolerancesTableSeeder');
     }
 }
