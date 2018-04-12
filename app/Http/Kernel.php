77901bf1ab10cc1e7c5c8c733d4c29d33c252753
AleksandrMishchaniuk <map.m@ua.fm>
2018-04-12 20:02:40 +0000

    added CORS middleware for API (disable CORS)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            'cors',
         ],
     ];
 
@@ -58,5 +59,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\Admin::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 }
