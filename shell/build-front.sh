140c9ba6a75080e57fddc2d0bca3e4d025ec089c
AleksandrMishchaniuk <map.m@ua.fm>
2018-06-28 22:34:44 +0300

    modified build-front.sh

--- shell/build-front.sh
+++ shell/build-front.sh
@@ -5,10 +5,9 @@ npm install --no-package-lock
 prod_env="production"
 dev_env="local"
 
-if [ $APP_ENV = $prod_env ]
-then
-    npm run prod
-elif [ $APP_ENV = $dev_env ]
+if [ $APP_ENV = $dev_env ]
 then
     npm run watch
+else
+    npm run prod
 fi
